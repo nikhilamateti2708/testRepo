506dedccdb6fde676e1047ce71a10e4cbe7768bf
nikhilamateti2708 <nanikki2000@gmail.com>
2021-09-16 16:13:32 +0530

    app.java update

--- LtiBank/src/main/java/com/lti/App.java
+++ LtiBank/src/main/java/com/lti/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!,Nikhila" );
-        System.out.println("hello Nikhila,hello");
+        System.out.println("hello Nikhila");
     }
 }
